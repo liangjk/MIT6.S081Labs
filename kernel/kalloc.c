58381f2f25d1441de3f065f24e1148f2d4a1cd89
liangjk <junkaili@usc.edu>
2024-01-27 06:26:58 +0000

    share lock among pageref on single page

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -23,25 +23,22 @@ struct {
   struct run *freelist;
 } kmem;
 
-struct pageref
-{
-  struct spinlock lock;
-  char cnt;
-} *pagerefbase;
-
 char *pagebase;
-
-struct pageref *getref(void *pa)
-{
-  return pagerefbase + ((char *)pa - pagebase) / PGSIZE;
-}
+#define REFPERPAGE ((PGSIZE - sizeof(struct spinlock)) / sizeof(char))
+#define REFLOCK(pa) ((struct spinlock *)PGROUNDDOWN(pa))
+#define REFONPAGE(page, idx) (((char *)(page) + sizeof(struct spinlock)) + idx)
 
 char addpageref(void *pa, char c)
 {
-  struct pageref *ref = getref(pa);
-  acquire(&ref->lock);
-  char ret = ref->cnt += c;
-  release(&ref->lock);
+  if (((uint64)pa % PGSIZE) != 0 || (char *)pa < end || (uint64)pa >= PHYSTOP)
+    panic("addpageref");
+  uint64 idx = ((char *)pa - pagebase) / PGSIZE;
+  uint64 pgn = idx / REFPERPAGE;
+  idx = idx % REFPERPAGE;
+  char *refcnt = REFONPAGE(pagebase + pgn * PGSIZE, idx);
+  acquire(REFLOCK((uint64)refcnt));
+  char ret = *refcnt += c;
+  release(REFLOCK((uint64)refcnt));
   return ret;
 }
 
@@ -55,15 +52,16 @@ kinit()
 void
 freerange(void *pa_start, void *pa_end)
 {
-  pagerefbase = (struct pageref *)pa_start;
   pagebase = (char *)PGROUNDUP((uint64)pa_start);
   int size = ((char *)pa_end - pagebase) / PGSIZE;
-  for (int i = 0; i < size; ++i)
+  char *p = pagebase;
+  for (int i = 0; i < size; i += REFPERPAGE)
   {
-    initlock(&pagerefbase[i].lock, "page refcnt");
-    pagerefbase[i].cnt = 1;
+    memset(p, 1, PGSIZE);
+    initlock((struct spinlock *)p, "page refcnt");
+    p += PGSIZE;
   }
-  for (char *p = (char *)PGROUNDUP((uint64)(pagerefbase + size)); p + PGSIZE <= (char *)pa_end; p += PGSIZE)
+  for (; p + PGSIZE <= (char *)pa_end; p += PGSIZE)
     kfree(p);
 }
 
@@ -74,20 +72,16 @@ freerange(void *pa_start, void *pa_end)
 void
 kfree(void *pa)
 {
-  struct pageref *ref = getref(pa);
-  acquire(&ref->lock);
-  if (ref->cnt > 1)
-  {
-    --ref->cnt;
-    release(&ref->lock);
-    return;
-  }
-  release(&ref->lock);
-
   struct run *r;
 
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
+
+  if (addpageref(pa, 0) > 1)
+  {
+    addpageref(pa, -1);
+    return;
+  }
 
   // Fill with junk to catch dangling refs.
   memset(pa, 1, PGSIZE);
